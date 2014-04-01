f76ebb5098d51f79f6ab3865b3d4dd6a89aefb0d
Olof Nilsson <olof.nilsson@findwise.com>
2014-04-01 11:41:35 +0200

    Reformat

--- admin-service/src/main/java/com/findwise/hydra/admin/rest/AppConfig.java
+++ admin-service/src/main/java/com/findwise/hydra/admin/rest/AppConfig.java
@@ -23,65 +23,65 @@ import com.findwise.hydra.mongodb.MongoType;
 @ComponentScan(basePackages = "com.findwise.hydra.admin.rest")
 public class AppConfig {
 
-	private static final String PROPERTIES_FILE = "admin-service.properties";
-	private static final String CONFIG_DIR_PROPERTY = "hydra.admin.config.dir";
+    private static final String PROPERTIES_FILE = "admin-service.properties";
+    private static final String CONFIG_DIR_PROPERTY = "hydra.admin.config.dir";
 
-	@Autowired
-	private DatabaseConfig databaseConfig;
+    @Autowired
+    private DatabaseConfig databaseConfig;
 
-	@Bean
-	@Autowired
-	public MongoConnector connector(DatabaseConfig config) {
-		return new MongoConnector(config);
-	}
+    @Bean
+    @Autowired
+    public MongoConnector connector(DatabaseConfig config) {
+        return new MongoConnector(config);
+    }
 
-	@Bean
-	public DatabaseConfig databaseConfig() {
-		return new DatabaseConfig();
-	}
+    @Bean
+    public DatabaseConfig databaseConfig() {
+        return new DatabaseConfig();
+    }
 
-	@Bean(name = "multipartResolver")
-	public CommonsMultipartResolver multipartResolver() {
-		CommonsMultipartResolver cmr = new CommonsMultipartResolver();
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver cmr = new CommonsMultipartResolver();
 
-		cmr.setMaxUploadSize(1024 * 1024 * 1024); // 1 Gigabyte...
+        cmr.setMaxUploadSize(1024 * 1024 * 1024); // 1 Gigabyte...
 
-		return cmr;
-	}
+        return cmr;
+    }
 
-        @Bean(name = "jsonpCallbackFilter")
-        public static JsonpCallbackFilter jsonpCallback(){
-            return new JsonpCallbackFilter();
-        }
+    @Bean(name = "jsonpCallbackFilter")
+    public static JsonpCallbackFilter jsonpCallback(){
+        return new JsonpCallbackFilter();
+    }
 
-	@Bean
-	public PropertyPlaceholderConfigurer properties() {
-		String configurationDirectory = System.getProperty(CONFIG_DIR_PROPERTY);
-		File propertiesFile = new File(configurationDirectory, PROPERTIES_FILE);
-		final Resource[] resources = new Resource[] {
-				new ClassPathResource(PROPERTIES_FILE),
-				new FileSystemResource(propertiesFile) };
-		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		ppc.setIgnoreUnresolvablePlaceholders(true);
-		ppc.setIgnoreResourceNotFound(true);
-		ppc.setLocations(resources);
-		return ppc;
-	}
+    @Bean
+    public PropertyPlaceholderConfigurer properties() {
+        String configurationDirectory = System.getProperty(CONFIG_DIR_PROPERTY);
+        File propertiesFile = new File(configurationDirectory, PROPERTIES_FILE);
+        final Resource[] resources = new Resource[] {
+                new ClassPathResource(PROPERTIES_FILE),
+                new FileSystemResource(propertiesFile) };
+        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+        ppc.setIgnoreUnresolvablePlaceholders(true);
+        ppc.setIgnoreResourceNotFound(true);
+        ppc.setLocations(resources);
+        return ppc;
+    }
 
-	@Bean
-	public ConfigurationService<MongoType> configurationService(
-			MongoConnector connector) {
-		return new ConfigurationService<MongoType>(connector);
-	}
+    @Bean
+    public ConfigurationService<MongoType> configurationService(
+            MongoConnector connector) {
+        return new ConfigurationService<MongoType>(connector);
+    }
 
-	@Bean
-	public DocumentsService<MongoType> documentsService(MongoConnector connector) {
-		return new DocumentsService<MongoType>(connector);
+    @Bean
+    public DocumentsService<MongoType> documentsService(MongoConnector connector) {
+        return new DocumentsService<MongoType>(connector);
 
-	}
+    }
 
-	@Bean
-	public StagesService<MongoType> stagesService(MongoConnector connector) {
-		return new StagesService<MongoType>(connector);
-	}
+    @Bean
+    public StagesService<MongoType> stagesService(MongoConnector connector) {
+        return new StagesService<MongoType>(connector);
+    }
 }
