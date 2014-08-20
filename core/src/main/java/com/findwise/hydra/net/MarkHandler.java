7c60f2ed93547f80e531c56d659d929036d8da00
Olof Nilsson <olof.nilsson@findwise.com>
2014-08-20 17:35:41 +0200

    Reformat

--- core/src/main/java/com/findwise/hydra/net/MarkHandler.java
+++ core/src/main/java/com/findwise/hydra/net/MarkHandler.java
@@ -21,108 +21,108 @@ import com.findwise.hydra.local.LocalDocument;
 import com.findwise.hydra.local.RemotePipeline;
 
 public class MarkHandler<T extends DatabaseType> implements ResponsibleHandler {
-	private enum Mark {
-		PENDING, PROCESSED, DISCARDED, FAILED
-	};
+    private enum Mark {
+        PENDING, PROCESSED, DISCARDED, FAILED
+    };
 
-	private static Logger logger = LoggerFactory.getLogger(MarkHandler.class);
+    private static Logger logger = LoggerFactory.getLogger(MarkHandler.class);
 
-	private CachingDocumentNIO<T> io;
-	private boolean performanceLogging = false;
-	
-	public MarkHandler(CachingDocumentNIO<T> io, boolean performanceLogging) {
-		this.io = io;
-		this.performanceLogging = performanceLogging;
-	}
+    private CachingDocumentNIO<T> io;
+    private boolean performanceLogging = false;
 
-	@Override
-	public void handle(HttpRequest request, HttpResponse response,
-			HttpContext context) throws HttpException, IOException {
-		long start = System.currentTimeMillis();
-		HttpEntity requestEntity = ((HttpEntityEnclosingRequest) request)
-				.getEntity();
-		String requestContent = EntityUtils.toString(requestEntity);
-		long tostring = System.currentTimeMillis();
-		String stage = RESTTools.getParam(request, RemotePipeline.STAGE_PARAM);
-		if (stage == null) {
-			HttpResponseWriter.printMissingParameter(response, RemotePipeline.STAGE_PARAM);
-			return;
-		}
+    public MarkHandler(CachingDocumentNIO<T> io, boolean performanceLogging) {
+        this.io = io;
+        this.performanceLogging = performanceLogging;
+    }
 
-		DatabaseDocument<T> databaseDocument;
-		try {
-			logger.debug("Received document '{}'", requestContent);
-			databaseDocument = io.convert(new LocalDocument(requestContent));
-		} catch (JsonException e) {
-			HttpResponseWriter.printJsonException(response, e);
-			return;
-		} catch (ConversionException e) {
-			logger.error("Caught Exception when trying to convert "+requestContent, e);
-			HttpResponseWriter.printBadRequestContent(response);
-			return;
-		}
-		long convert = System.currentTimeMillis();
-		Mark requestedMark = getMark(request);
-		logger.debug("Stage '{}' requested Mark '{}' for document with id '{}'", stage, requestedMark, databaseDocument.getID());
-		boolean markWasSuccessful = mark(databaseDocument, stage, requestedMark);
-		long mark = System.currentTimeMillis();
-		if (markWasSuccessful) {
-			HttpResponseWriter.printSaveOk(response, databaseDocument.getID());
-		} else {
-			HttpResponseWriter.printNoDocument(response); // Failure does not always mean the document is missing
-		}
-		if (performanceLogging) {
-			long end = System.currentTimeMillis();
-			logger.info(String.format("type=performance event=processed stage_name=%s doc_id=%s start=%d end=%d total=%d entitystring=%d parse=%d mark=%d serialize=%d",
-					stage, databaseDocument.getID(), start, end, end-start, tostring-start, convert-tostring, mark-convert, end-mark));
-		}
-	}
-	
-	private Mark getMark(HttpRequest request) {
-		String uri = RESTTools.getBaseUrl(request);
-		if (uri.equals(RemotePipeline.PROCESSED_DOCUMENT_URL)) {
-			return Mark.PROCESSED;
-		} else if (uri.equals(RemotePipeline.PENDING_DOCUMENT_URL)) {
-			return Mark.PENDING;
-		} else if (uri.equals(RemotePipeline.DISCARDED_DOCUMENT_URL)) {
-			return Mark.DISCARDED;
-		} else if (uri.equals(RemotePipeline.FAILED_DOCUMENT_URL)) {
-			return Mark.FAILED;
-		}
-		return null;
-	}
+    @Override
+    public void handle(HttpRequest request, HttpResponse response,
+                       HttpContext context) throws HttpException, IOException {
+        long start = System.currentTimeMillis();
+        HttpEntity requestEntity = ((HttpEntityEnclosingRequest) request)
+                .getEntity();
+        String requestContent = EntityUtils.toString(requestEntity);
+        long tostring = System.currentTimeMillis();
+        String stage = RESTTools.getParam(request, RemotePipeline.STAGE_PARAM);
+        if (stage == null) {
+            HttpResponseWriter.printMissingParameter(response, RemotePipeline.STAGE_PARAM);
+            return;
+        }
 
-	private boolean mark(DatabaseDocument<T> databaseDocument, String stage, Mark mark) throws IOException {
-		logger.trace("handleMark(..., ..., " + mark.toString() + ")");
+        DatabaseDocument<T> databaseDocument;
+        try {
+            logger.debug("Received document '{}'", requestContent);
+            databaseDocument = io.convert(new LocalDocument(requestContent));
+        } catch (JsonException e) {
+            HttpResponseWriter.printJsonException(response, e);
+            return;
+        } catch (ConversionException e) {
+            logger.error("Caught Exception when trying to convert " + requestContent, e);
+            HttpResponseWriter.printBadRequestContent(response);
+            return;
+        }
+        long convert = System.currentTimeMillis();
+        Mark requestedMark = getMark(request);
+        logger.debug("Stage '{}' requested Mark '{}' for document with id '{}'", stage, requestedMark, databaseDocument.getID());
+        boolean markWasSuccessful = mark(databaseDocument, stage, requestedMark);
+        long mark = System.currentTimeMillis();
+        if (markWasSuccessful) {
+            HttpResponseWriter.printSaveOk(response, databaseDocument.getID());
+        } else {
+            HttpResponseWriter.printNoDocument(response); // Failure does not always mean the document is missing
+        }
+        if (performanceLogging) {
+            long end = System.currentTimeMillis();
+            logger.info(String.format("type=performance event=processed stage_name=%s doc_id=%s start=%d end=%d total=%d entitystring=%d parse=%d mark=%d serialize=%d",
+                    stage, databaseDocument.getID(), start, end, end - start, tostring - start, convert - tostring, mark - convert, end - mark));
+        }
+    }
 
-		switch (mark) {
-		case PENDING: {
-			return io.markPending(databaseDocument, stage);
-		}
-		case PROCESSED: {
-			return io.markProcessed(databaseDocument, stage);
-		}
-		case FAILED: {
-			return io.markFailed(databaseDocument, stage);
-		}
-		case DISCARDED: {
-			return io.markDiscarded(databaseDocument, stage);
-		}
-		}
-		return false;
-	}
+    private Mark getMark(HttpRequest request) {
+        String uri = RESTTools.getBaseUrl(request);
+        if (uri.equals(RemotePipeline.PROCESSED_DOCUMENT_URL)) {
+            return Mark.PROCESSED;
+        } else if (uri.equals(RemotePipeline.PENDING_DOCUMENT_URL)) {
+            return Mark.PENDING;
+        } else if (uri.equals(RemotePipeline.DISCARDED_DOCUMENT_URL)) {
+            return Mark.DISCARDED;
+        } else if (uri.equals(RemotePipeline.FAILED_DOCUMENT_URL)) {
+            return Mark.FAILED;
+        }
+        return null;
+    }
 
-	@Override
-	public boolean supports(HttpRequest request) {
-		return RESTTools.isPost(request) && getMark(request)!=null;
-	}
+    private boolean mark(DatabaseDocument<T> databaseDocument, String stage, Mark mark) throws IOException {
+        logger.trace("handleMark(..., ..., " + mark.toString() + ")");
 
-	@Override
-	public String[] getSupportedUrls() {
-		return new String[] { RemotePipeline.DISCARDED_DOCUMENT_URL,
-				RemotePipeline.FAILED_DOCUMENT_URL,
-				RemotePipeline.PROCESSED_DOCUMENT_URL,
-				RemotePipeline.PENDING_DOCUMENT_URL };
-	}
+        switch (mark) {
+            case PENDING: {
+                return io.markPending(databaseDocument, stage);
+            }
+            case PROCESSED: {
+                return io.markProcessed(databaseDocument, stage);
+            }
+            case FAILED: {
+                return io.markFailed(databaseDocument, stage);
+            }
+            case DISCARDED: {
+                return io.markDiscarded(databaseDocument, stage);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean supports(HttpRequest request) {
+        return RESTTools.isPost(request) && getMark(request)!=null;
+    }
+
+    @Override
+    public String[] getSupportedUrls() {
+        return new String[] { RemotePipeline.DISCARDED_DOCUMENT_URL,
+                RemotePipeline.FAILED_DOCUMENT_URL,
+                RemotePipeline.PROCESSED_DOCUMENT_URL,
+                RemotePipeline.PENDING_DOCUMENT_URL };
+    }
 
 }
